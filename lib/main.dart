6afb089acf9584320c02b239dfb1c35c9986e29d
Aayan Wasim Raja <aayanraja@Aayans-MacBook-Pro.local>
2020-01-09 12:16:45 -0500

    Final Commit... Hopefully2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:weather_gci/weatherPage.dart';
 
-void main() => runApp(WeatherApp());
+void main() { 
+  
+  ErrorWidget.builder = (FlutterErrorDetails details) => Container(
+    child: Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+  runApp(WeatherApp());
+
+}
 
 
